cc07003aff0661cc398a2c23c7027b1b2de02b56
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2023-03-24 17:24:47 +0000

    preprocessing completed: begin scaling to gpu

--- data_preprocessing/derive_features/derive_features_cpu.py
+++ data_preprocessing/derive_features/derive_features_cpu.py
@@ -44,98 +44,10 @@ from constant import (
     GENERATED_COLS,
     ENCODED_COLS
 )
-def _encode_countries(x):
-        if x == 'Finland':
-            return 1
-        elif x == 'United States':
-            return 2
-        elif x == 'China':
-            return 3
-        else:
-            return 4
-
 def get_logger():
     logger = logging.getLogger(__name__)
     return logger
 
-
-
-def join_for_encodings(df):
-
-
-    project_id_value_counts = df['project_id'].value_counts().reset_index().rename(columns={'index': 'project_id', 'project_id': 'project_count'})
-    df = df.merge(project_id_value_counts, on='project_id', how='left')
-
-    if torch.cuda.is_available():
-        df = df.compute().to_pandas()
-    else:
-        df = df.compute()
-
-    df['country'] = df['country'].apply(_encode_countries)
-
-    if torch.cuda.is_available():
-        df = pd.from_pandas(df)
-
-    df['date_time'] = pd.to_datetime(df['date_time'])
-    total_events = df['user_id'].value_counts().reset_index().rename(columns={'index': 'user_id', 'user_id': 'total_events'})
-
-    df = df.merge(total_events, on='user_id', how='left')
-    df['cum_events'] = df[['user_id', 'task_within_session_count']].groupby('user_id').cumcount() + 1
-    df['cum_projects'] = (df.groupby('user_id')['project_id'].transform(lambda x: pd.CategoricalIndex(x).codes) + 1).astype('int32')
-    df['cum_time'] = df.groupby('user_id')['time_diff_seconds'].cumsum()
-    df['cum_time_within_session'] = df.groupby(['user_id', '30_minute_session_count'])['time_diff_seconds'].cumsum()
-    df['av_time_across_clicks'] = df.groupby('user_id')['time_diff_seconds'].rolling(1000, min_periods=1).mean().reset_index()['time_diff_seconds']
-    df['av_time_across_clicks_session'] = df.groupby(['user_id', '30_minute_session_count'])['time_diff_seconds'].rolling(10, min_periods=1).mean().reset_index()['time_diff_seconds']
-
-
-    av_num_events_within_session = df.groupby(['user_id', '30_minute_session_count'])['task_within_session_count'].max().reset_index()
-    av_num_events_within_session['rolling_average_tasks_within_session'] = av_num_events_within_session.groupby('user_id')['task_within_session_count'].rolling(10, min_periods=1).mean().reset_index()['task_within_session_count']
-
-    av_time_within_session = df.groupby(['user_id', '30_minute_session_count']).apply(lambda x: x['date_time'].max() - x['date_time'].min()).reset_index().rename(columns={0: 'av_time_within_session'})
-    av_time_within_session['av_time_within_session'] = av_time_within_session['av_time_within_session'].apply(lambda x: x.total_seconds())
-    av_time_within_session['rolling_av_time_within_session'] = av_time_within_session.groupby('user_id')['av_time_within_session'].rolling(10, min_periods=1).mean().reset_index()['av_time_within_session']
-
-
-    session_start_time = df.groupby(['user_id', '30_minute_session_count'])['date_time'].min().reset_index().rename(columns={'date_time': 'session_start_time'})
-    session_end_time = df.groupby(['user_id', '30_minute_session_count'])['date_time'].max().reset_index().rename(columns={'date_time': 'session_end_time'})
-
-    session_meta = session_start_time.merge(session_end_time, on=['user_id', '30_minute_session_count'], how='left')
-    session_meta['previous_end_time'] = session_meta.groupby(['user_id'])['session_end_time'].shift()
-    session_meta['previous_end_time'] = session_meta['previous_end_time'].fillna(0)
-    session_meta['time_between_sessions'] = session_meta.apply(lambda x: 0 if x['previous_end_time'] == 0 else (x['session_start_time'] - x['previous_end_time']), axis=1)
-    session_meta['time_between_sessions'] = session_meta['time_between_sessions'].apply(lambda x: 0 if x == 0 else x.seconds)
-    session_meta['rolling_time_between_sessions'] = session_meta.groupby('user_id')['time_between_sessions'].rolling(5,  min_periods=1).mean().reset_index()['time_between_sessions']
-
-    df = df.merge(av_num_events_within_session[['user_id', '30_minute_session_count', 'rolling_average_tasks_within_session']], on=['user_id', '30_minute_session_count'], how='left')
-    df = df.merge(av_time_within_session[['user_id', '30_minute_session_count', 'rolling_av_time_within_session']], on=['user_id', '30_minute_session_count'], how='left')
-    df = df.merge(session_meta[['user_id', '30_minute_session_count', 'rolling_time_between_sessions']], on=['user_id', '30_minute_session_count'], how='left')
-    user_id_hash = pd.DataFrame(df['user_id'].unique()).reset_index().rename(columns={'index': 'user_id_hash', 0: 'user_id'})
-    project_id_hash = pd.DataFrame(df['project_id'].unique()).reset_index().rename(columns={'index': 'project_id_hash', 0: 'project_id'})
-
-    user_id_hash['user_id_hash'] = user_id_hash['user_id_hash'] + 1
-    project_id_hash['project_id_hash'] = project_id_hash['project_id_hash'] + 1
-
-    df = df.merge(user_id_hash, on='user_id', how='left')
-    df = df.merge(project_id_hash, on='project_id', how='left')
-
-    df = df.drop(columns=['user_id', 'project_id'])
-    df = df.rename(columns={'user_id_hash': 'user_id', 'project_id_hash': 'project_id'})
-    return df
-
-def prepare_for_sessionization(data_paths: list, scaler: MinMaxScaler):
-
-    df = dd.read_csv(data_paths, usecols=INITIAL_LOAD_COLUMNS)
-
-    df = join_for_encodings(df)
-    df['timestamp_raw'] = df['date_time'].astype('int64') // 10**9
-    df = df.sort_values(by='date_time')
-
-    df['timestamp'] = df['timestamp_raw']
-    print(f'Loaded data: shape = {df.shape}, min_date, max_date: {df.date_time.min()}, {df.date_time.max()}')
-    print(f'Data label true: {df.label.value_counts() / len(df)}')
-    df = df[SCALED_COLS + GENERATED_COLS + ENCODED_COLS + ['timestamp_raw', 'label', 'total_events']]
-    df[SCALED_COLS + GENERATED_COLS] = scaler.fit_transform(df[SCALED_COLS + GENERATED_COLS].values)
-    return df.astype('float32')
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -161,6 +73,9 @@ def encode_counts(df):
     df = df.merge(user_count, on='user_id')
     df = df.merge(project_count, on='project_id')
     df = df.merge(country_count, on='country')
+    
+    df = df.drop(columns=['user_id', 'project_id', 'country'])
+    df = df.rename(columns={'user_id_hash': 'user_id', 'project_id_hash': 'project_id', 'country_hash': 'country'})
     return df
    
 def time_encodings(df):
@@ -217,14 +132,100 @@ def intra_session_stats(df, logger):
     df['delta_last_event'] = df.groupby(['user_id', 'session_30'])['date_time'].diff()
     df['delta_last_event'] = df['delta_last_event'].dt.total_seconds()
     df['delta_last_event'] = df['delta_last_event'].fillna(0)
-    df['cum_session_time_seconds'] = df.groupby(['user_id', 'session_30'])['delta_last_event'].cumsum()
+    df['cum_session_time_minutes'] = df.groupby(['user_id', 'session_30'])['delta_last_event'].cumsum()
+    df['cum_session_time_minutes'] = df['cum_session_time_minutes'] / 60
     logger.info('Beginning rolling window 10 calculation')
-    df = rolling_window_session_10(df, logger)
     logger.info('Rolling window 10 calculation complete: beginning expanding window calculation')
-    df = expanding_session_time_delta(df, logger)
     logger.info('Expanding window calculation complete: returning to dask')
     return df
 
+
+def running_user_stats(df, logger):
+    logger.info('Calculating cumulative platform time')
+    df['cum_platform_time_minutes'] = df.groupby(['user_id'])['delta_last_event'].cumsum()
+    df['cum_platform_time_minutes'] = df['cum_platform_time_minutes'] / 60
+    logger.info('Calculating cumulative platform events')
+    df['cum_platform_events'] = df.groupby(['user_id'])['delta_last_event'].cumcount() + 1
+    logger.info('Calculated cumulative platform events: calculating running unique projects')
+    df['cum_projects'] = (df.groupby('user_id')['project_id'].transform(lambda x: pd.CategoricalIndex(x).codes) + 1).astype('int32')
+    logger.info('Calculated running unique projects: calculating average event time delta')
+    df = df.reset_index()
+    df['row_count'] = df.index.values
+    
+    average_event_time = df.set_index('row_count') \
+        .groupby('user_id') \
+        .expanding(min_periods=1)['delta_last_event'].mean() \
+        .reset_index().rename(columns={'delta_last_event': 'average_event_time'}) \
+        .sort_values(by='row_count')
+    df = df.set_index('row_count').join(average_event_time[['row_count', 'average_event_time']].set_index('row_count'))
+    logger.info('Calculated average event time delta')
+    return df
+
+def expanding_session_time(df, session_inflection_times, logger):
+ 
+    logger.info('Session inflection times calculated: calculating expanding session time')
+    session_inflection_times['time_in_session_minutes'] = (session_inflection_times['date_time_session_end'] - session_inflection_times['date_time_session_start']).dt.total_seconds() / 60
+   
+    session_inflection_times['expanding_session_time_minutes'] = session_inflection_times \
+        .groupby(['user_id'])['time_in_session_minutes'] \
+        .rolling(10000, min_periods=1, closed='left') \
+        .mean() \
+        .reset_index() \
+        .rename(columns={'time_in_session_minutes': 'expanding_session_time_minutes'})['expanding_session_time_minutes']
+    
+    logger.info('Expanding session time calculated: joining to df')
+  
+    df = pd.merge(df, session_inflection_times[['user_id', 'session_30', 'expanding_session_time_minutes']], on=['user_id', 'session_30'], how='left')
+    df['expanding_session_time_minutes'] = df['expanding_session_time_minutes'].fillna(0)
+    logger.info('Expanding session time joined to df')
+    return df
+
+def time_between_sessions(df, session_min_max, logger):
+    
+    session_min_max['previous_session_end'] = session_min_max.groupby(['user_id'])['date_time_session_end'].shift(1)
+    
+    session_min_max['previous_session_end'] = (
+        session_min_max[['previous_session_end', 'date_time_session_start']] \
+            .apply(lambda x: x['previous_session_end'] if pd.notnull(x['previous_session_end']) else x['date_time_session_start'], axis=1)
+    )
+
+    session_min_max['time_between_sessions_minutes'] = (session_min_max['date_time_session_start'] - session_min_max['previous_session_end']).dt.total_seconds() / 60
+    
+    n_events =  session_min_max[session_min_max['session_30'] > 1]
+    
+    logger.info('Running aggregations on time between sessions')
+    expanding_time_between_sesssion_minutes = n_events \
+        .set_index(['date_time_session_start', 'session_30']) \
+        .sort_values(by=['session_30', 'user_id']) \
+        .groupby(['user_id'])['time_between_sessions_minutes'] \
+        .expanding(1) \
+        .mean() \
+        .reset_index() \
+        .rename(columns={'time_between_sessions_minutes': 'expanding_session_delta_minutes'}) 
+        
+    
+    logger.info('Aggregations complete: joining to df')
+       
+    df = pd.merge(df, expanding_time_between_sesssion_minutes[['user_id', 'session_30', 'expanding_session_delta_minutes']], on=['user_id', 'session_30'], how='left') 
+    df['expanding_session_delta_minutes'] = df['expanding_session_delta_minutes'].fillna(0)
+       
+    logger.info('Time between sessions joined to df')
+    df = df.sort_values(by='date_time')
+    
+    logger.info('DF resorted by date_time')
+    return df
+
+def assign_metadata(df, logger):
+    logger.info(f'Obtaining global session time and user events')
+    global_events_user = df.groupby('user_id')['cum_platform_events'].max().reset_index().rename(columns={'cum_platform_events': 'global_events_user'})
+    global_session_time = df.groupby('user_id')['cum_platform_time_minutes'].max().reset_index().rename(columns={'cum_platform_time_minutes': 'global_session_time_minutes'})
+    
+    metadata = global_events_user.set_index('user_id').join(global_session_time.set_index('user_id'))
+     
+    logger.info('Joining metadata to df')
+    df = pd.merge(df, metadata, on='user_id', how='left')
+    return df
+    
 def main(args):
     #
     torch.set_printoptions(sci_mode=False)
@@ -239,9 +240,11 @@ def main(args):
 
     files = glob.glob(f'{args.input_path}/*.parquet')
     files = list(sorted(files))
-    logger.info(f'Found {len(files)} files in {args.input_path}: {files[:args.data_subset]}')
+    logger.info(f'Found {len(files)} files in {args.input_path}')
+    logger.info(pformat(f'Loading data from'))
+    logger.info(pformat(files[:args.data_subset]))
     df = dd.read_parquet(files[:args.data_subset], usecols=INITIAL_LOAD_COLUMNS)
-    logger.info(f'Loaded data: shape = {df.shape}, min_date, max_date: {df.date_time.min()}, {df.date_time.max()}')
+    logger.info(f'Loaded data: shape = {df.shape}, min_date, max_date: {df.date_time.min().compute()}, {df.date_time.max().compute()}')
     df = df[INITIAL_LOAD_COLUMNS]
     df['date_time'] = dd.to_datetime(df['date_time'])
     logger.info(f'Sorting data by date_time')
@@ -258,34 +261,57 @@ def main(args):
     df['user_id'] = df['user_id'].astype('category')
     
     
+    logger.info('Categorical features encoded: calculating intra-session stats')
     df = intra_session_stats(df, logger)
-    sample_df = df[df['user_id'] == 2371513].sort_values(by='date_time')
-    print(sample_df[['user_id', 'session_30', 'date_time', 'delta_last_event', 'rolling_10', 'expanding_click_average']].head(100))
+    logger.info('Beginning rolling window 10 calculation')
     
+    df = rolling_window_session_10(df, logger)
+    logger.info('Rolling window 10 calculation complete: beginning expanding window calculation')
+    
+    df = expanding_session_time_delta(df, logger)
+    logger.info('Expanding window calculation complete: returning to dask')
+    
+    logger.info(f'Calculating running user stats')
+    df = running_user_stats(df, logger)
+    
+    logger.info('Calculating between session stats')
+   
+    session_inflection_times = df.groupby(['user_id', 'session_30']).agg({'date_time': ['min', 'max']}).reset_index().rename(columns={'min': 'session_start', 'max': 'session_end'})
+    session_inflection_times.columns = session_inflection_times.columns.map('_'.join).str.strip()
+    session_inflection_times = session_inflection_times.rename(columns={'user_id_': 'user_id', 'session_30_': 'session_30'})
+    
+    logger.info('Session inflection times calculated: columns')
+    logger.info(pformat(session_inflection_times.columns))
+ 
+    logger.info('Calculating time within session')
+    df = expanding_session_time(df, session_inflection_times, logger)
 
+    logger.info('Calculating time between sessions')
+    df = time_between_sessions(df, session_inflection_times, logger)
+    
+    df['session_30_raw'] = df['session_30']
 
-    # df = prepare_for_sessionization(files, MinMaxScaler())
-    # print('Dataframe columns:')
-    # pp.pprint(list(df.columns))
+    logger.info('Assigning metadata')
+    df = assign_metadata(df, logger)
+    logger.info('Metadata assigned: dropping columns')
+    
+    logger.info('Returning df to dask for writing to disk')
+    
+    df = dd.from_pandas(df, npartitions=10)
+   
+    output_path = os.path.join(args.output_path, f'files_used_{args.data_subset}')
+    logger.info(f'Writing to {output_path}')
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)    
 
-    # for seq in args.seq_list:
-    #     output_path = os.path.join(
-    #         args.output_path,
-    #         f'files_used_{args.data_subset}',
-    #     )
-    #     sessionize = SessionizeData(df, seq, output_path, save_s3=args.save_s3)
-    #     sessionize.generate_sequence()
-    #     print(f'Finished writing {seq} to disk')
-    # print(f'Exiting application...')
-
-
-
+    df = df.sort_values(by='date_time').reset_index(drop=True).to_parquet(output_path, engine='pyarrow', compression='snappy', write_index=False)
+    logger.info('Finished writing to disk')
 
 class Arguments:
     def __init__(self):
         self.input_path = 'datasets/labelled_session_count_data/'
         self.output_path = 'datasets/calculated_features/'
-        self.data_subset = 5
+        self.data_subset = 2
 
 
 if __name__ == "__main__":
